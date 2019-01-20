88b3ce1c83e36edcc494f3eeade75043a9697491
Кирилл Анисимов <akpoflash@MacBook-Pro-Kirill.local>
2019-01-20 20:46:53 +0300

    Add sketch of left menu with profile info

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { loadState, saveState } from '~/helpers/localStorage';
+import { AGES, SEX, ANSWERS, ACTIONS } from '~/constants/profile';
+
+const StyledProfile = styled.form`
+
+`;
+
+const Title = styled.h4`
+	margin: 0;
+`;
+
+const List = styled.ul`
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	font-size: 0.8rem;
+`;
+
+class Profile extends React.PureComponent {
+	state = {
+		age: null,
+		sex: null,
+		answer: null,
+		action: [],
+	}
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		let checkedActions = [];
+		event.target.action.forEach(
+			(elem, index) => (
+				checkedActions[index] = elem.checked ? elem.value : null
+				)
+		);
+		this.setState({
+			age: event.target.age.value,
+			sex: event.target.sex.value,
+			answer: event.target.answer.value,
+			action: checkedActions,
+		});
+
+	}
+
+	componentDidMount(){
+		this.setState( loadState() );
+	}
+
+	componentDidUpdate(){
+		saveState(this.state);
+	}
+
+	render(){
+		return (
+			<StyledProfile onSubmit={ this.handleSubmit }>
+				<Title>Пожалуйста заполните анкету</Title>
+				<List>
+					<Title>Ваш Возраст</Title>
+					{
+						AGES.map((value, index) => (
+							<li key={ index }>
+								<input 
+									required
+									type='radio' 
+									name='age' 
+									value={ value }
+									defaultValue={ this.state.age }
+									id={`age${ index }`} />
+								<label htmlFor={`age${ index }`}>{ value }</label>
+							</li>
+						))
+					}
+				</List>
+				<List>
+					<Title>Пол</Title>
+					{
+						SEX.map((value, index) => (
+							<li key={ index }>
+								<input
+									required
+									type='radio'
+									name='sex'
+									value={ value }
+									id={`sex${ index }`} />
+								<label htmlFor={`sex${index}`}>{ value }</label>
+							</li>
+						))
+					}
+				</List>
+				<List>
+					<Title>Вы проживаете на территории Северо-Запада?</Title>
+					{
+						ANSWERS.map((value, index) => (
+							<li key={ index }>
+								<input
+									required
+									type='radio'
+									name='answer'
+									value={ value }
+									id={`answer${ index }`} />
+								<label htmlFor={`answer${ index }`}>{ value }</label>
+							</li>
+						))
+					}
+				</List>
+				<List>
+					<Title>На Северо-Западе Вы:</Title>
+					{
+						ACTIONS.map((value, index) => (
+							<li key={index}>
+								<input
+									type='checkbox'
+									name='action'
+									value={ value }
+									id={`action${index}`} />
+								<label htmlFor={`action${index}`}>{value}</label>
+							</li>
+						))
+					}
+				</List>
+				<input 
+					type='submit'
+					value='Сохранить' />
+			</StyledProfile>
+		);
+	}
+}
+
+export default Profile;
