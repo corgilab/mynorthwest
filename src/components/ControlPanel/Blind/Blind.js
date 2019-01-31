53213dcac86ab7ac89af36353d2e40b119446dc9
Kirill Anisimov <kirill.anisimov@stoloto.ru>
2019-01-31 15:00:34 +0300

    Changed behavior for mobile/desctop devices

--- src/components/ControlPanel/Blind/Blind.js
+++ src/components/ControlPanel/Blind/Blind.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { MOBILE_SIZE } from '~/constants/common';
 import { BACKGROUND_COLOR, BORDER_COLOR } from '~/constants/styles';
 
 const StyledBlind = styled.span`
-	width: 20px;
-	height: 40px;
+	width: 25px;
+	height: 50px;
 	background-color: ${ BACKGROUND_COLOR};
 	position: absolute;
 	z-index: 10;
-	top: calc(50% - 10px);
-	right: -20px;
+	top: calc(50% - 25px);
+	right: -25px;
 	border-right: 1px solid ${ BORDER_COLOR};
 	border-top: 1px solid ${ BORDER_COLOR};
 	border-bottom: 1px solid ${ BORDER_COLOR};
+
+	@media (max-width: ${`${MOBILE_SIZE}px`}){
+		right: ${ props => props.isOpen ? '0' : '-25px'};
+		border-left: ${ props => props.isOpen ? `1px solid ${ BORDER_COLOR }` : 'none' };
+	}
 `;
 
 const Arrow = styled.img`
@@ -24,7 +30,7 @@ const Arrow = styled.img`
 
 const Blind = (props) => {
     return (
-        <StyledBlind onClick={ props.handleBlindClick }>
+		<StyledBlind isOpen={ props.isOpen } onClick={ props.handleBlindClick }>
             <Arrow isOpen={ props.isOpen } src="/resources/images/arrow.svg" />
         </StyledBlind>
     );
