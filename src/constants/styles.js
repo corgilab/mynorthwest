b1d0d9aa8720439c67ae6b39810f0e3c53daed47
Kirill Anisimov <kirill.anisimov@stoloto.ru>
2019-02-13 19:02:16 +0300

    Added extra points

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,4 +1,4 @@
-export const BACKGROUND_COLOR = '#A1A1A1A1';
+export const BACKGROUND_COLOR = '#A1A1A1BB';
 export const BORDER_COLOR = '#ccc';
 export const MAIN_COLOR = '#A79AE5';
 export const FONT_COLOR = '#111';
